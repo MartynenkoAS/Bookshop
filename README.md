596b258a1268ff6fe184dbb0eb1d507a854fcf62
Alexander Martynenko <MartynenkoAS@yandex.ru>
2024-02-25 19:01:50 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Проект: «Книжный интернет-магазин на API Google Books»
+
+Ссылка на Git: https://github.com/MartynenkoAS/Bookshop.git
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+const path                 = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin   = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin    = require("html-webpack-plugin");
+const ESLintWebpackPlugin  = require("eslint-webpack-plugin");
+const StylelintPlugin      = require("stylelint-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'main.js'
+    },
+    mode: 'development',                                // после окончания разработки поменять на "production" для минификации бандла JS
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+                        template: "./src/index.pug",
+                        filename: "index.html"}),
+        new ESLintWebpackPlugin({fix: true}),
+        new StylelintPlugin({fix: true})
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.pug$/,
+                loader: "pug-loader",
+                options: {pretty: true}                         // после окончания разработки поменять на false для минификации файла html
+            }
+        ],
+    },
+    optimization: {
+        minimizer: [`...`, new CssMinimizerPlugin()],
+      },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+    },
+};
